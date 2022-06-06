f847cd0bf77ef1013ec8a0fd05cda0d89ec0d1d8
Mustafeez001 <mustafeezahmad071@gmail.com>
2022-06-06 20:55:00 +0530

    evaluation

---
+++ record.js
@@ -0,0 +1,55 @@
+// fill in javascript code here
+
+document.querySelector("form").addEventListener("submit",myFunction) 
+ 
+function myFunction(event){ 
+    event.preventDefault() 
+let name=document.getElementById("name").value 
+let id=document.getElementById("employeeID").value 
+let depart=document.getElementById("department").value 
+let exp=document.getElementById("exp").value 
+let mail=document.getElementById("email").value 
+let mobile=document.getElementById("mbl").value 
+let tr=document.createElement("tr") 
+ 
+let td1=document.createElement("td") 
+td1.innerText=name; 
+ 
+let td2=document.createElement("td") 
+td2.innerText=id; 
+ 
+let td3=document.createElement("td") 
+td3.innerText=depart 
+ 
+let td4=document.createElement("td") 
+td4.innerText=exp 
+ 
+let td5=document.createElement("td") 
+td5.innerText=mail 
+ 
+let td6=document.createElement("td") 
+td6.innerText=mobile 
+ 
+let td7=document.createElement("td") 
+if(exp>5){ 
+    td7.innerText="Senior" 
+}else if(exp<5||exp>2){ 
+    td7.innerText="Junior" 
+}else if(exp<=1){ 
+    td7.innerText= "Fresher" 
+} 
+ 
+let td8=document.createElement("td") 
+td8.innerText="DELETE" 
+td8.addEventListener("click",function(del){ 
+    del.target.parentNode.remove(); 
+ 
+}) 
+ 
+tr.append(td1,td2,td3,td4,td5,td6,td7,td8) 
+document.querySelector("tbody").appendChild(tr); 
+console.log(name,id,depart,exp,mail,mobile); 
+ 
+}
+
+/* Fill CSS here 
